8f228ec63a07a84b57f853d5284b532542adf051
AndreConforti <andre.conforti@nappsolutions.com>
2024-02-23 00:37:13 -0300

    created register page

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ app_name = 'account'
 
 urlpatterns = [
     path('log_in', views.log_in, name='log_in'),
+    path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
 
 
 def log_in(request):
-    pass
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        return HttpResponse(f'{email} | {password}')
+
+
+def register(request):
+    return render(request, 'register.html')
